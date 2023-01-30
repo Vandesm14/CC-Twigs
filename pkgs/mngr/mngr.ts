79d276c9b21ef5085567386a1bdda7b0ce6f9029
Shane Vandegrift <vandesm14@gmail.com>
2023-01-29 19:13:27 -0500

    server list for mngr

--- pkgs/mngr/mngr.ts
+++ pkgs/mngr/mngr.ts
@@ -1,1 +1,32 @@
 const args = [...$vararg];
+
+function getServerList() {
+  // read from ".mngr/serverlist.txt", a list of servers separated by newline
+  const dirExists = fs.exists('.mngr/serverlist.txt');
+  if (!dirExists) {
+    const [file] = fs.open('.mngr/serverlist.txt', 'w');
+    if (!file) {
+      print('Failed to create serverlist.txt');
+      shell.exit();
+    }
+
+    file.write('http://mr.thedevbird.com:3000/pkgs');
+    file.close();
+
+    print('Added default server to serverlist.txt');
+  }
+
+  const [file] = fs.open('.mngr/serverlist.txt', 'r');
+  if (!file) {
+    print('Failed to open serverlist.txt');
+    shell.exit();
+  }
+
+  const servers = file.readAll().split('\n');
+  file.close();
+
+  return servers;
+}
+
+print('Mngr - Package Manager');
+print(getServerList().join());
