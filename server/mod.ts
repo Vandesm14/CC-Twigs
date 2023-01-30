7b2a3e626c3c43c038f41ec3132282aaabea35b2
Shane Vandegrift <vandesm14@gmail.com>
2023-01-29 20:44:20 -0500

    server: watch for changes

--- server/mod.ts
+++ server/mod.ts
@@ -2,14 +2,27 @@ import * as fs from 'https://deno.land/std@0.175.0/fs/walk.ts';
 import { Server } from 'https://deno.land/std@0.175.0/http/server.ts';
 
 const PATH = Deno.args[0] || '.';
-const entries: string[] = [];
 
-for await (const entry of fs.walk(PATH, {
-  exts: ['.lua', '.txt'],
-  maxDepth: 2,
-})) {
-  entries.push(entry.path);
+async function debounce(fn: () => any, time: number) {
+  let timeout: number;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(fn, time);
+  };
 }
+
+async function getEntries() {
+  const entries = [];
+  for await (const entry of fs.walk(PATH, {
+    exts: ['.lua', '.txt'],
+    maxDepth: 2,
+  })) {
+    entries.push(entry.path);
+  }
+  return entries;
+}
+
+let entries: string[] = await getEntries();
 
 const port = 3000;
 const handler = (req: Request) => {
@@ -42,4 +55,15 @@ const handler = (req: Request) => {
 const server = new Server({ port, handler });
 
 console.log(`Server running on port: ${port}`);
+
+const watcher = Deno.watchFs(PATH);
+const debouncer = await debounce(async () => {
+  console.log('Change detected, updating entries...');
+  entries = await getEntries();
+}, 1000);
+
+for await (const event of watcher) {
+  debouncer();
+}
+
 await server.listenAndServe();
