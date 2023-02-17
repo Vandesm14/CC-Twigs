a2ebff205b3b3b846fc7a3039e1f61b7ccf70ea6
Shane Vandegrift <vandesm14@gmail.com>
2023-02-16 20:45:23 -0500

    [mngr]: fix installer and other bugs

--- pkgs/mngr/install.ts
+++ pkgs/mngr/install.ts
@@ -2,15 +2,14 @@ export const address =
   settings.get('mngr.address') ?? 'http://mr.thedevbird.com:3000/pkgs';
 
 function downloadFile(file: string) {
-  shell.run(
-    'wget',
-    `${address}/mngr/${file}.lua`,
-    `.mngr/lib/mngr/${file}.lua`
-  );
+  shell.run('wget', `${address}/mngr/${file}`, `.mngr/lib/mngr/${file}`);
 }
 
-downloadFile('mngr');
-downloadFile('api');
-downloadFile('file');
+downloadFile('mngr.lua');
+downloadFile('api.lua');
+downloadFile('file.lua');
+downloadFile('usage.txt');
+downloadFile('README.md');
+downloadFile('pkg.json');
 
 shell.run('.mngr/lib/mngr/mngr.lua install mngr');
