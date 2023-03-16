99ef0b505677917df859a473308a736fa9020ac6
Shane Vandegrift <vandesm14@gmail.com>
2023-03-16 16:57:01 -0400

    [turt]: remove absolute facings from test

--- pkgs/turt/test.ts
+++ pkgs/turt/test.ts
@@ -7,8 +7,8 @@ turt.move(2, 'backward');
 turt.move(1, 'left');
 turt.move(2, 'right');
 
-turt.moveTo(0, 0, 0, 'north'); // Origin
+turt.moveTo(0, 0, 0); // Origin
 turt.moveTo(-2, 0, 2); // Computers
-turt.moveTo(0, 0, 0, 'north'); // Origin
+turt.moveTo(0, 0, 0); // Origin
 turt.moveTo(-8, 0, -11); // House
-turt.moveTo(0, 0, 0, 'north'); // Origin
+turt.moveTo(0, 0, 0); // Origin
