cac2bf9c8e4b2e797ea88c3b5bc726feb1e4eece
Shane Vandegrift <vandesm14@gmail.com>
2023-03-16 14:13:58 -0400

    add turning and facing

--- pkgs/turt/turt.ts
+++ pkgs/turt/turt.ts
@@ -1,12 +1,21 @@
 type CompassDirection = 'north' | 'east' | 'south' | 'west';
 type RelativeDirection = 'forward' | 'right' | 'back' | 'left';
 
+type Position = {
+  x: number;
+  y: number;
+  z: number;
+  heading: CompassDirection;
+};
+
 export const directions: {
   compass: CompassDirection[];
   relative: RelativeDirection[];
+  turns: RelativeDirection[][];
 } = {
   compass: ['north', 'east', 'south', 'west'],
   relative: ['forward', 'right', 'back', 'left'],
+  turns: [[], ['right'], ['right', 'right'], ['left']],
 };
 
 export class Relative {
@@ -33,5 +42,44 @@ export class Relative {
     const relativeIndex = directions.relative.indexOf(move);
 
     return directions.compass[(index + relativeIndex) % 4] as CompassDirection;
+  }
+
+  compassToRelative(move: CompassDirection): RelativeDirection {
+    const facing = this.heading;
+    const index = directions.compass.indexOf(facing);
+    const compassIndex = directions.compass.indexOf(move);
+
+    return directions.relative[
+      (compassIndex - index + 4) % 4
+    ] as RelativeDirection;
+  }
+
+  face(direction: CompassDirection) {
+    const turns =
+      directions.turns[
+        directions.relative.indexOf(this.compassToRelative(direction))
+      ]!;
+    turns.forEach((turn) =>
+      turn === 'left' ? turtle.turnLeft() : turtle.turnRight()
+    );
+
+    this.heading = direction;
+  }
+
+  turn(direction: RelativeDirection) {
+    const turns = directions.turns[directions.relative.indexOf(direction)]!;
+    turns.forEach((turn) =>
+      turn === 'left' ? turtle.turnLeft() : turtle.turnRight()
+    );
+    this.heading = this.relativeToCompass(direction);
+  }
+
+  position(): Position {
+    return {
+      x: this.x,
+      y: this.y,
+      z: this.z,
+      heading: this.heading,
+    };
   }
 }
