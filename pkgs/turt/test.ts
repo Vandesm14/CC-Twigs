fbc8bacf27c85b7a645825e7148d08a7be0e7ffc
Shane Vandegrift <vandesm14@gmail.com>
2023-03-16 14:42:31 -0400

    update test program

--- pkgs/turt/test.ts
+++ pkgs/turt/test.ts
@@ -1,3 +1,19 @@
 import { Turtle } from './turt';
 
 const turt = new Turtle({ x: 0, y: 0, z: 0, heading: 'north' });
+
+turt.move(1, 'forward');
+
+print(turt.position());
+
+turt.move(2, 'backward');
+
+print(turt.position());
+
+turt.move(1, 'left');
+
+print(turt.position());
+
+turt.move(2, 'right');
+
+print(turt.position());
