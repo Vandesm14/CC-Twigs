b6ed327d1bdd8135fd33c3795647204bc368dff5
Vandesm14 <vandesm14@gmail.com>
2023-10-09 22:38:37 +0000

    add to Replit

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+  deps = [
+    pkgs.yarn
+    pkgs.esbuild
+    pkgs.nodejs-18_x
+
+    pkgs.nodePackages.typescript
+    pkgs.nodePackages.typescript-language-server
+    pkgs.deno
+    pkgs.nodePackages.pnpm
+  ];
+}
