3073c4bf9c59b185571269ae53d6e778a0bb96f9
Vandesm14 <vandesm14@gmail.com>
2023-10-10 03:41:10 +0000

    add softlink

--- pkgs/net/send.ts
+++ pkgs/net/send.ts
@@ -1,21 +1,31 @@
-import peripheral, { PeripheralKind } from 'cc/peripheral';
-import { BASE } from 'bgp/api';
+import softlink from "./softlink";
 
 const args = [...$vararg];
-const destination_ = args[0];
-const via_ = args[1];
+const data = tostring(args[0]);
+const destinations = args.slice(1).map(Number);
 
-if (destination_ === undefined) throw 'No destination provided';
-if (via_ === undefined) throw 'No message provided';
+const modem = peripheral.find("modem")[0]! as ModemPeripheral;
 
-const destination = parseInt(destination_);
-const via = parseInt(via_);
+softlink.send(modem, { destinations, data });
 
-if (isNaN(destination)) throw 'Destination is not a number';
-if (isNaN(via)) throw 'Via is not a number';
+// import peripheral, { PeripheralKind } from 'cc/peripheral';
+// import { BASE } from 'bgp/api';
 
-const message = BASE.create(destination, via);
+// const args = [...$vararg];
+// const destination_ = args[0];
+// const via_ = args[1];
 
-peripheral
-  .find(PeripheralKind.Modem)
-  .forEach((modem) => modem.transmit(1, 2, message));
+// if (destination_ === undefined) throw 'No destination provided';
+// if (via_ === undefined) throw 'No message provided';
+
+// const destination = parseInt(destination_);
+// const via = parseInt(via_);
+
+// if (isNaN(destination)) throw 'Destination is not a number';
+// if (isNaN(via)) throw 'Via is not a number';
+
+// const message = BASE.create(destination, via);
+
+// peripheral
+//   .find(PeripheralKind.Modem)
+//   .forEach((modem) => modem.transmit(1, 2, message));
