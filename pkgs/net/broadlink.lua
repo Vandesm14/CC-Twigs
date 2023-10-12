255c09743478716e92713e5ae82f978c624b231a
Leon Davis <leonskidev@pm.me>
2023-10-13 00:11:31 +0100

    better protocol strategy

--- pkgs/net/broadlink.lua
+++ pkgs/net/broadlink.lua
@@ -1,78 +1,101 @@
+local pretty = require("cc.pretty")
+local render, pretty = pretty.render, pretty.pretty
+
 --- A data link (OSI layer 2) protocol.
 ---
---- This is an unreliable and unencrypted protocol for broadcast modem-to-modem
---- communication.
+--- Broadlink is an unreliable and unencrypted protocol
 local broadlink = {
-    --- The unique broadlink protcol ID.
-    pid = 1036,
+  --- The unique Broadlink protocol ID.
+  pid = 1036,
+  --- The Broadlink OS event name.
+  event = "broadlink",
 }
 
---- Sends a broadlink frame.
+--- Sends a Broadlink data frame.
 ---
 --- @generic T : table
+--- @param side computerSide
+--- @param channel integer
 --- @param data T
-function broadlink.send(data)
-    os.queueEvent(
-        "modem_message",
-        nil,
-        broadlink.pid,
-        broadlink.pid,
-        { broadlink.pid, os.getComputerID(), data }
-    )
+function broadlink.send(side, channel, data)
+  os.queueEvent(
+    "modem_message",
+    side,
+    channel,
+    channel,
+    { broadlink.pid, channel, os.getComputerID(), data }
+  )
 end
 
+--- Receives a Broadlink data frame.
+---
+--- This pauses execution on the current thread.
+---
+--- @return computerSide side
+--- @return integer channel
+--- @return integer source
+--- @return table data
+function broadlink.recv()
+  --- @diagnostic disable-next-line
+  local _, side, channel, source, data = os.pullEvent(broadlink.event)
+  return side, channel, source, data
+end
+
+--- Handles the Broadlink protocol.
+---
+--- Takes control of the execution on the current thread.
 function broadlink.daemon()
-    --- @type unknown, computerSide?, unknown, unknown, unknown
-    local _, side, _, _, payload = os.pullEvent("modem_message")
+  while true do
+    --- @type event, computerSide, integer, integer, unknown
+    local _, side, _, _, frame = os.pullEvent("modem_message")
 
-    -- 1. If all of the following are true.
     if
-        type(payload) == "table"
-        -- - PID is the broadlink PID.
-        and payload[1] == broadlink.pid
+    -- - Frame is a table.
+        type(frame) == "table"
+        -- - PID is the Broadlink PID.
+        and frame[1] == broadlink.pid
+        -- - Channel is a number.
+        and type(frame[2]) == "number"
         -- - Source is a number.
-        and type(payload[2]) == "number"
+        and type(frame[3]) == "number"
         -- - Data is a table.
-        and type(payload[3]) == "table"
+        and type(frame[4]) == "table"
     then
-        -- 2. Then payload is a valid broadlink frame.
+      -- 1. If the above are true, this is a valid Broadlink data frame.
 
-        --- @type any, number, table
-        local _, source, data = table.unpack(payload)
+      --- @type integer, integer, table
+      local channel, source, data = table.unpack(frame, 2)
 
-        -- 3. If The source is this computer.
-        if source == os.getComputerID() then
-            -- 4. Then this broadlink frame should be sent from this computer
-            --    via all connected modems, except the one it was received on.
-            for _, name in ipairs(peripheral.getNames()) do
-                if name ~= side and peripheral.getType(name) == "modem" then
-                    peripheral.wrap(name).transmit(
-                        broadlink.pid,
-                        broadlink.pid,
-                        { broadlink.pid, source, data }
-                    )
-                end
-            end
-        else
-            -- 5. Otherwise, the broadlink frame should be received by this
-            --    computer.
-            os.queueEvent(
-                "modem_message",
-                side,
-                broadlink.pid,
-                broadlink.pid,
-                data
-            )
+      if source == os.getComputerID() then
+        -- 2. If the source is this computer, send the data frame.
+        if peripheral.getType(side) == "modem" then
+          local modem = peripheral.wrap(side)
+          --- SAFETY: This is known to be true since we check above.
+          --- @cast modem Modem
+          modem.transmit(channel, channel, {
+            broadlink.pid,
+            channel,
+            source,
+            data
+          })
+
+          print("SEND:", side, channel, source, render(pretty(data)))
         end
+      else
+        -- 3. Otherwise the destination is this computer, consume the data frame
+        --    and re-queue it as a Broadlink event.
+        os.queueEvent(broadlink.event, side, channel, source, data)
+
+        print("RECV:", side, channel, source, render(pretty(data)))
+      end
     end
+  end
 end
 
 if not package.loaded["net.broadlink"] then
-    -- This file was run as an executable.
-    while true do
-        broadlink.daemon()
-    end
+  -- This file was run as an executable.
+  broadlink.daemon()
 else
-    -- This file was loaded as a library.
-    return broadlink
+  -- This file was loaded as a library.
+  return broadlink
 end
--- pkgs/net/unilink.lua
+++ pkgs/net/unilink.lua
@@ -1,83 +1,105 @@
+local pretty = require("cc.pretty")
+local render, pretty = pretty.render, pretty.pretty
+
 --- A data link (OSI layer 2) protocol.
 ---
---- This is an unreliable and unencrypted protocol for unicast modem-to-modem
---- communication.
+--- Unilink is an unreliable and unencrypted protocol
 local unilink = {
-    --- The unique unilink protcol ID.
-    pid = 1035,
+  --- The unique Unilink protocol ID.
+  pid = 1035,
+  --- The Unilink OS event name.
+  event = "unilink",
 }
 
---- Sends a unilink frame.
+--- Sends a Unilink data frame.
 ---
 --- @generic T : table
+--- @param side computerSide
+--- @param channel integer
 --- @param destination integer
 --- @param data T
-function unilink.send(destination, data)
-    os.queueEvent(
-        "modem_message",
-        nil,
-        unilink.pid,
-        unilink.pid,
-        { unilink.pid, os.getComputerID(), destination, data }
-    )
+function unilink.send(side, channel, destination, data)
+  os.queueEvent(
+    "modem_message",
+    side,
+    channel,
+    channel,
+    { unilink.pid, channel, os.getComputerID(), destination, data }
+  )
 end
 
+--- Receives a Unilink data frame.
+---
+--- This pauses execution on the current thread.
+---
+--- @return computerSide side
+--- @return integer channel
+--- @return integer source
+--- @return table data
+function unilink.recv()
+  --- @diagnostic disable-next-line
+  local _, side, channel, source, data = os.pullEvent(unilink.event)
+  return side, channel, source, data
+end
+
+--- Handles the Unilink protocol.
+---
+--- Takes control of the execution on the current thread.
 function unilink.daemon()
-    --- @type unknown, computerSide?, unknown, unknown, unknown
-    local _, side, _, _, payload = os.pullEvent("modem_message")
+  while true do
+    --- @type event, computerSide, integer, integer, unknown
+    local _, side, _, _, frame = os.pullEvent("modem_message")
 
-    -- 1. If all of the following are true.
     if
-        type(payload) == "table"
-        -- - PID is the unilink PID.
-        and payload[1] == unilink.pid
+    -- - Frame is a table.
+        type(frame) == "table"
+        -- - PID is the Unilink PID.
+        and frame[1] == unilink.pid
+        -- - Channel is a number.
+        and type(frame[2]) == "number"
         -- - Source is a number.
-        and type(payload[2]) == "number"
+        and type(frame[3]) == "number"
         -- - Destination is a number.
-        and type(payload[3]) == "number"
+        and type(frame[4]) == "number"
         -- - Data is a table.
-        and type(payload[4]) == "table"
+        and type(frame[5]) == "table"
     then
-        -- 2. Then payload is a valid unilink frame.
+      -- 1. If the above are true, this is a valid Unilink data frame.
 
-        --- @type any, number, number, table
-        local _, source, destination, data = table.unpack(payload)
+      --- @type integer, integer, integer, table
+      local channel, source, destination, data = table.unpack(frame, 2)
 
-        -- 3. If The source is this computer.
-        if source == os.getComputerID() then
-            -- 4. Then this unilink frame should be sent from this computer via
-            --    all connected modems, except the one it was received on.
-            for _, name in ipairs(peripheral.getNames()) do
-                if name ~= side and peripheral.getType(name) == "modem" then
-                    peripheral.wrap(name).transmit(
-                        unilink.pid,
-                        unilink.pid,
-                        { unilink.pid, source, destination, data }
-                    )
-                end
-            end
-        elseif destination == os.getComputerID() then
-            -- 5. Otherwise, the unilink frame should be received by this
-            --    computer.
-            os.queueEvent(
-                "modem_message",
-                side,
-                unilink.pid,
-                unilink.pid,
-                data
-            )
-        -- c. Otherwise, the destination is not this computer.
+      if source == os.getComputerID() then
+        -- 2. If the source is this computer, send the data frame.
+        if peripheral.getType(side) == "modem" then
+          local modem = peripheral.wrap(side)
+          --- SAFETY: This is known to be true since we check above.
+          --- @cast modem Modem
+          modem.transmit(channel, channel, {
+            unilink.pid,
+            channel,
+            source,
+            destination,
+            data
+          })
+
+          print("SEND:", side, channel, source, destination, render(pretty(data)))
         end
-    end
-end
+      elseif destination == os.getComputerID() then
+        -- 3. If the destination is this computer, consume the data frame and
+        --    re-queue it as a Unilink event.
+        os.queueEvent(unilink.event, side, channel, source, data)
 
+        print("RECV:", side, channel, source, destination, render(pretty(data)))
+      end
+    end
+  end
+end
 
 if not package.loaded["net.unilink"] then
-    -- This file was run as an executable.
-    while true do
-        unilink.daemon()
-    end
+  -- This file was run as an executable.
+  unilink.daemon()
 else
-    -- This file was loaded as a library.
-    return unilink
+  -- This file was loaded as a library.
+  return unilink
 end
