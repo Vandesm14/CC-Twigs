8f8b5b94b2467b42065149801c90962b7b0baf23
Vandesm14 <vandesm14@gmail.com>
2023-10-15 01:09:50 +0000

    update hey

--- pkgs/hey/hey.lua
+++ pkgs/hey/hey.lua
@@ -7,12 +7,6 @@ local exit = false
 
 term.clear()
 
-for _, side in ipairs(peripheral.getNames()) do
-  if peripheral.getType(side) == "modem" then
-    peripheral.wrap(side).open(0)
-  end
-end
-
 local function displayMessages()
   for i = 1, termHeight - 1 do
     term.setCursorPos(1, (termHeight - 1) - i)
@@ -27,19 +21,6 @@ end
 while not exit do
   parallel.waitForAny(
     function()
-      os.pullEventRaw("terminate")
-      exit = true
-
-      for _, side in ipairs(peripheral.getNames()) do
-        if peripheral.getType(side) == "modem" then
-          peripheral.wrap(side).close(0)
-        end
-      end
-
-      term.clear()
-      term.setCursorPos(1, 1)
-    end,
-    function()
       term.setCursorPos(1, termHeight)
       term.clearLine()
       term.write("Chat: ")
@@ -50,14 +31,14 @@ while not exit do
 
       for _, side in ipairs(peripheral.getNames()) do
         if peripheral.getType(side) == "modem" then
-          broadlink.send(side, 0, { message })
+          broadlink.transmit(side, { message })
         end
       end
 
       displayMessages()
     end,
     function()
-      local _, _, source, data = broadlink.recv()
+      local _, source, data = broadlink.receive()
       if type(data[1]) == "string" then
         messages[#messages + 1] = { from = source, message = data[1] }
       end
