c674a49898a7c06c207f13a6e9435c455fc15219
Leon Davis <leonskidev@pm.me>
2023-10-15 19:28:26 +0100

    refactor mngr into its simplest form

--- pkgs/mngr/mngr.lua
+++ pkgs/mngr/mngr.lua
@@ -1,191 +1,142 @@
---- Provides package manager APIs.
-local mngr = {}
-
---- The base directory where all packages are stored.
-mngr.baseDir = ".mngr"
-
---- Returns the URL parsed from the 'mngr.addr' setting.
----
---- @return string|nil url
-function mngr.getUrlFromSettings()
-  --- @type string|nil
-  local url = settings.get("mngr.url")
-  if url == nil or not http.checkURL(url) then return end
-  return url
+--- @param s string
+--- @return string
+local function trimSlash(s)
+  if s:sub(s:len()) == "/" then
+    s = s:sub(1, s:len() - 1)
+  end
+  return s
 end
 
---- Returns the directory for a package.
----
---- @return string packageDir The package directory.
-function mngr.getPackageDir(package)
-  return fs.combine(mngr.baseDir, package)
-end
-
---- Fetch the list of packages.
----
---- @param baseUrl string
---- @return string[]|nil packages
-function mngr.fetchPackages(baseUrl)
-  local res = http.get(baseUrl)
-  if res == nil then return end
+--- @param rootUrl string
+--- @return string[]|nil packageNames
+local function fetchPackages(rootUrl)
+  local response = http.get(rootUrl, nil, false)
+  if response == nil then return end
 
   --- @type string[]
   local names = {}
 
-  local name = res.readLine(false)
+  local name = response.readLine(false)
   while name ~= nil do
     names[#names + 1] = name
-    name = res.readLine(false)
+    name = response.readLine(false)
   end
 
+  response.close()
   return names
 end
 
---- Fetch the list of files in a package.
----
---- @param baseUrl string
---- @param package string
---- @return string[]|nil files
-function mngr.fetchPackageFiles(baseUrl, package)
-  local url = baseUrl .. "/" .. package
-  if url == nil then return end
+--- @param rootUrl string
+--- @param packageName string
+--- @return string[]|nil fileNames
+local function fetchPackageFiles(rootUrl, packageName)
+  local url = rootUrl .. "/" .. packageName
 
-  local res = http.get(url)
-  if res == nil then return end
+  local response = http.get(url, nil, false)
+  if response == nil then return end
 
   --- @type string[]
   local names = {}
 
-  local name = res.readLine(false)
+  local name = response.readLine(false)
   while name ~= nil do
     names[#names + 1] = name
-    name = res.readLine(false)
+    name = response.readLine(false)
   end
 
+  response.close()
   return names
 end
 
---- Fetch the list of dependency packages for a package file.
----
---- @param baseUrl string
---- @param package string
---- @param file string
---- @return string[]|nil packages
-function mngr.fetchPackageFileDependencies(baseUrl, package, file)
-  local url = baseUrl .. "/" .. package .. "/" .. file .. "/deps"
-  if url == nil then return end
+--- @param rootUrl string
+--- @param packageName string
+--- @param fileName string
+--- @return string|nil fileContent
+local function fetchPackageFileContent(rootUrl, packageName, fileName)
+  local url = rootUrl .. "/" .. packageName .. "/" .. fileName
 
-  local res = http.get(url)
-  if res == nil then return end
+  local response = http.get(url, nil, false)
+  if response == nil then return end
 
-  --- @type string[]
-  local names = {}
-
-  local name = res.readLine(false)
-  while name ~= nil do
-    names[#names + 1] = name
-    name = res.readLine(false)
-  end
-
-  return names
-end
-
---- Fetch the contents of a package file.
----
---- @param baseUrl string
---- @param package string
---- @param file string
---- @return string|nil content
-function mngr.fetchPackageFile(baseUrl, package, file)
-  local url = baseUrl .. "/" .. package .. "/" .. file
-  if url == nil then return end
-
-  local res = http.get(url)
-  if res == nil then return end
-
-  return res.readAll()
-end
-
---- Installs a package and its dependencies recursively.
----
---- @param baseUrl string
---- @param package string
---- @return boolean success
-function mngr.installPackage(baseUrl, package)
-  local files = mngr.fetchPackageFiles(baseUrl, package)
-  if files == nil then return false end
-
-  fs.makeDir(mngr.getPackageDir(package))
-  local packageDir = mngr.getPackageDir(package)
-
-  for _, file in ipairs(files) do
-    local content = mngr.fetchPackageFile(baseUrl, package, file)
-    if content == nil then return false end
-
-    local filePath = fs.combine(packageDir, file)
-    local handle = fs.open(filePath, "w")
-    if handle == nil then return false end
-
-    handle.write(content)
-    handle.flush()
-
-    handle.close()
-
-    local dependencyPackages = mngr.fetchPackageFileDependencies(
-      baseUrl,
-      package,
-      file
-    ) or {}
-
-    for _, dependencyPackage in ipairs(dependencyPackages) do
-      local dependencyPackageSuccess
-      = mngr.installPackage(baseUrl, dependencyPackage)
-
-      if not dependencyPackageSuccess then return false end
-      break
-    end
-  end
-
-  return true
+  local content = response.readAll()
+  response.close()
+  return content
 end
 
 if not package.loaded["mngr.mngr"] then
-  -- This file was run as an executable.
-  -- TODO: All of the stuff that mngr did, but better.
-  -- TODO: Provide two modes, remote-to-computer and computer-to-computer.
+  local mngrDir = "/.mngr"
 
-  -- Update all installed packages.
-  local url = mngr.getUrlFromSettings()
-  if url == nil then
-    printError(
-      "Be sure to set the 'mngr.url' setting to the package server URL. For " ..
-      "example, 'set mngr.url http://localhost:3000'."
-    )
+  local tempDir = fs.combine("/.temp", mngrDir)
+  if not pcall(fs.makeDir, tempDir) then
+    printError("Unable to create '" .. tempDir .. "' directory.")
     return
   end
 
-  local packages = mngr.fetchPackages(url)
+  local rootUrl = settings.get("mngr.url") or ""
+  rootUrl = trimSlash(rootUrl)
 
-  if packages == nil then
-    printError("Unable to fetch packages from '" .. url .. "'.")
+  if not http.checkURL(rootUrl) then
+    printError("Expected the 'mngr.url' setting to be a valid URL.")
     return
   end
 
-  -- Delete the .mgnr directory.
-  fs.delete(mngr.baseDir)
+  local packageNames = fetchPackages(rootUrl)
+  if packageNames == nil then
+    printError("Unable to fetch packages.")
+    return
+  end
 
-  -- Create the .mngr directory.
-  fs.makeDir(mngr.baseDir)
+  for _, packageName in ipairs(packageNames) do
+    local packageDir = fs.combine(tempDir, packageName)
 
-  for _, package in ipairs(packages) do
-    local success = mngr.installPackage(url, package)
-    if success then
-      print("Updated '" .. package .. "' and its dependencies.")
-    else
-      -- TODO: This also occurs when the package was a dependency of a package
-      --       that was previously installed. Filter those out so that this
-      --       doesn't happen.
-      printError("Unable to update '" .. package .. "' and its dependencies.")
+    local fileNames = fetchPackageFiles(rootUrl, packageName)
+    if fileNames == nil then
+      printError("Unable to fetch files for package '" .. packageName .. "'.")
+      return
     end
+
+    print("Fetching package '" .. packageName .. "'...")
+
+    for _, fileName in ipairs(fileNames) do
+      local fileContent = fetchPackageFileContent(rootUrl, packageName, fileName)
+      if fileContent == nil then
+        printError("Unable to fetch file '" .. fs.combine(packageName, fileName) .. "'.")
+        return
+      end
+
+      local filePath = fs.combine(packageDir, fileName)
+
+      local file = fs.open(filePath, "w")
+      if file == nil then
+        printError("Unable to create file '" .. filePath .. "'.")
+        return
+      end
+
+      print("  Fetched '" .. fileName .. "'.")
+
+      file.write(fileContent)
+      file.close()
+    end
+  end
+
+  print("Committing from '" .. tempDir .. "' to '" .. mngrDir .. "'...")
+
+  fs.delete(mngrDir)
+  fs.move(tempDir, mngrDir)
+  fs.delete(tempDir)
+
+  print("Done.")
+
+  shell.setPath( shell.path() .. ":/.mngr/mngr")
+
+  if not fs.exists("/startup/mngr.lua") then
+    local file = fs.open("/startup/mngr.lua", "w")
+    if not file then
+      printError("Unable to create '/startup/mngr.lua' file.")
+      return
+    end
+
+    file.writeLine("shell.setPath( shell.path() .. \":\" .. \"/.mngr/mngr\")")
+    file.close()
   end
 end
