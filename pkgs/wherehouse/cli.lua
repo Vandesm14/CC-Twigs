75d54b85db9f88c1ec996a039db3ea76cd28817c
Vandesm14 <vandesm14@gmail.com>
2023-10-17 03:21:03 +0000

    make wherehouse local-only

--- pkgs/wherehouse/cli.lua
+++ pkgs/wherehouse/cli.lua
@@ -1,8 +1,20 @@
-local pretty = require("cc.pretty")
-local follownet = require("net.follownet")
-local server = require("wherehouse.server")
+--- @param chest Inventory
+--- @param table table
+local function countItems(chest, table)
+  -- Run through each item in the chest
+  for _, item in pairs(chest.list()) do
+    local name, count = item.name, item.count
 
-local usage = "Usage: " .. arg[0] .. " <order|ls>"
+    -- Update or set the entry
+    if table[name] ~= nil then
+      table[name] = table[name] + count
+    else
+      table[name] = count
+    end
+  end
+end
+
+local usage = "Usage: " .. arg[0] .. " <order|ls|capacity>"
 local command = arg[1]
 
 if command == nil then
@@ -13,28 +25,86 @@ if command == nil then
 elseif command == "ls" then
   local query = arg[2]
 
-  follownet.transmit({ 3, 4 }, { 3524, { 3, os.getComputerID() }, "list" })
+  local items = {}
 
-  local _, _, packet = follownet.receive()
-  local pid, path, type_, data = table.unpack(packet)
+  -- Scan each chest for items, until we hit the end-stop
+  --- @diagnostic disable-next-line: param-type-mismatch
+  for _, chest in ipairs({ peripheral.find("minecraft:chest") }) do
+    --- @cast chest Inventory
 
-  if
-      pid == server.pid
-      and type(path) == "table"
-      and type(type_) == "string"
-      and type(data) == "table"
-  then
-    if type_ == "list" then
-      print("Items:")
-      for name, count in pairs(data) do
-        if query ~= nil and type(query) == "string" then
-          if string.find(name, query) ~= nil then
-            print(name .. ": " .. count)
-          end
-        else
-          print(name .. ": " .. count)
+    if chest ~= nil then
+      countItems(chest, items)
+    end
+  end
+
+  print("Items:")
+  for name, count in pairs(items) do
+    if query ~= nil and type(query) == "string" then
+      if string.find(name, query) ~= nil then
+        print(name .. ": " .. count)
+      end
+    else
+      print(name .. ": " .. count)
+    end
+  end
+elseif command == "pull" then
+  local query = arg[2]
+
+  --- The distribution chest MUST be a single chest
+  --- and the storage chests MUST be double chests
+  --- @diagnostic disable-next-line: param-type-mismatch
+  local distributionChest = peripheral.find("minecraft:barrel")
+  --- @cast distributionChest Inventory
+
+  if distributionChest == nil then
+    printError("No distribution chest found.")
+    return true
+  end
+
+  local items = {}
+  countItems(distributionChest, items)
+  local myItems = {}
+  for key, val in pairs(items) do
+    myItems[key] = val
+  end
+
+  local distributionChestName = peripheral.getName(distributionChest)
+
+  -- Run through each stack and try to place it in a chest
+  for slot, item in pairs(distributionChest.list()) do
+    --- @diagnostic disable-next-line: param-type-mismatch
+    for _, chest in ipairs({ peripheral.find("minecraft:chest") }) do
+      --- @cast chest Inventory
+      local chestName = peripheral.getName(chest)
+
+      -- Move the stack into the chest
+      local success, got = pcall(
+        chest.pullItems,
+        distributionChestName,
+        slot
+      )
+
+      -- If we succeed, then decrement how many items we have left
+      if success then
+        print("sent", got, "of", item.name, "to", chestName)
+        myItems[item.name] = myItems[item.name] - got
+
+        -- If we clear out all of our items, skip to the next item
+        if got == myItems[item.name] then
+          break
         end
       end
+    end
+  end
+
+  print("Pulled:")
+  for name, count in pairs(items) do
+    if query ~= nil and type(query) == "string" then
+      if string.find(name, query) ~= nil then
+        print(name .. ": " .. count)
+      end
+    else
+      print(name .. ": " .. count)
     end
   end
 elseif command == "order" then
@@ -59,22 +129,81 @@ elseif command == "order" then
     [item] = amount
   }
 
-  print("Order:")
-  pretty.pretty_print(order)
+  --- @type table<string, integer>
+  local myItems = {}
 
-  follownet.transmit({ 3, 4 }, { 3524, { 3, os.getComputerID() }, "order", order })
+  --- The distribution chest MUST be a single chest
+  --- and the storage chests MUST be double chests
+  --- @diagnostic disable-next-line: param-type-mismatch
+  local distributionChest = peripheral.find("minecraft:barrel")
+  --- @cast distributionChest Inventory
 
-  local _, _, packet = follownet.receive()
-  local pid, path, type_, data = table.unpack(packet)
+  if distributionChest == nil then
+    printError("No distribution chest found.")
+    return true
+  end
 
-  if
-      pid == server.pid
-      and type(path) == "table"
-      and type(type_) == "string"
-      and type(data) == "table" then
-    print("Found:")
-    for name, count in pairs(data) do
-      print(name .. ": " .. count)
+  -- Add 0's for all the order items
+  for name, _ in pairs(order) do
+    myItems[name] = 0
+  end
+
+  -- Scan each chest for items, until we hit the end-stop
+  --- @diagnostic disable-next-line: param-type-mismatch
+  for _, chest in ipairs({ peripheral.find("minecraft:chest", function(_, chest) return chest.size() == 54 end) }) do
+    --- @cast chest Inventory
+    local chestName = peripheral.getName(chest)
+    print("Found chest '" .. chestName .. "'...")
+
+    -- Check for items. Run through each item
+    for slot, item in pairs(chest.list()) do
+      if item ~= nil then
+        -- If the order wants this item...
+        if order[item.name] ~= nil then
+          local need = order[item.name] - myItems[item.name]
+
+          -- ...and we don't have enough
+          if need > 0 then
+            -- Move the items into our distribution chest
+            local success, got = pcall(
+              distributionChest.pullItems,
+              chestName,
+              slot,
+              need
+            )
+
+            if success then
+              -- Add this action to our running totals
+              myItems[item.name] = myItems[item.name] + got
+              print(
+                "  Got",
+                tostring(got),
+                item.name .. ", need",
+                tostring(need - got),
+                "more."
+              )
+            else
+              printError("  Unable to pull from it.")
+              break
+            end
+          end
+        end
+      end
     end
   end
+
+  print("Found:")
+  for name, count in pairs(myItems) do
+    print(name .. ": " .. count)
+  end
+elseif command == "capacity" then
+  local capacity = 0
+
+  --- @diagnostic disable-next-line: param-type-mismatch
+  for _, chest in ipairs({ peripheral.find("minecraft:chest") }) do
+    --- @cast chest Inventory
+    capacity = capacity + chest.size()
+  end
+
+  print("The capacity is:", capacity)
 end
--- pkgs/wherehouse/startup.lua
+++
@@ -1,9 +0,0 @@
-local scheduler = require("scheduler.daemon")
-
-scheduler.schedules[#scheduler.schedules + 1] = { runner = require("net.unilink").schedule, logs = {} }
-scheduler.schedules[#scheduler.schedules + 1] = { runner = require("net.broadlink").schedule, logs = {} }
-scheduler.schedules[#scheduler.schedules + 1] = { runner = require("net.follownet").schedule, logs = {} }
-scheduler.schedules[#scheduler.schedules + 1] = { runner = require("net.searchnet").schedule, logs = {} }
-scheduler.schedules[#scheduler.schedules + 1] = { runner = require("wherehouse.server").schedule, logs = {} }
-
-scheduler.run()
