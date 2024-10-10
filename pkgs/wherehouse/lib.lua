a8992ea7523b695ea56d34e32426b4ea596cebac
Shane <vandesm14@gmail.com>
2024-10-10 16:33:23 -0400

    parse coordinates

--- pkgs/wherehouse/lib.lua
+++ pkgs/wherehouse/lib.lua
@@ -20,10 +20,23 @@ local function getName(chest)
   return nil
 end
 
+--- @class Position
+--- @field x number
+--- @field y number
+--- @field z number
+Position = {}
+
 --- @class Chest
 --- @field name string
 --- @field items itemList
+--- @field position Position
 Chest = {}
+
+-- Chest ID = `c{x}_{y}_{z}`
+local function parseCoordinates(str)
+  local x, y, z = str:match("c(%d+)_(%d+)_(%d+)")
+  return { x = tonumber(x), y = tonumber(y), z = tonumber(z) }
+end
 
 function lib.scanItems()
   --- @type table<number, Chest>
@@ -31,7 +44,7 @@ function lib.scanItems()
 
   -- Scan each chest for items, until we hit the end-stop
   --- @diagnostic disable-next-line: param-type-mismatch
-  for i, chest in ipairs({ peripheral.find("minecraft:chest") }) do
+  for _, chest in ipairs({ peripheral.find("minecraft:chest") }) do
     --- @cast chest Inventory
 
     if chest ~= nil then
@@ -39,7 +52,8 @@ function lib.scanItems()
       if name ~= nil then
         table.insert(chests, {
           name = name,
-          items = chest.list()
+          items = chest.list(),
+          position = parseCoordinates(name)
         })
       end
     end
