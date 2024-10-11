7ee1b1a80a37c226dda17f2691da05f09bde7fa1
Shane <vandesm14@gmail.com>
2024-10-10 21:03:53 -0400

    fix ignore system

--- pkgs/turt/auto.lua
+++ pkgs/turt/auto.lua
@@ -12,26 +12,26 @@ local ignore = 0
 while true do
   turtle.forward()
 
-  if ignore > 0 then
-    ignore = ignore - 1
-    turtle.forward()
-  end
-
   local isBlock, info = turtle.inspectDown()
   if isBlock and info then
     local color = getColor(info.tags)
-    if color == "white" then
-      turtle.turnRight()
-    elseif color == "black" then
-      turtle.turnLeft()
-    elseif color == "yellow" then
-      ignore = ignore + 1
-    elseif color == "purple" then
-      break
-    elseif color == "blue" then
-      break
-    elseif color == "red" then
-      break
+
+    if color ~= nil and ignore > 0 then
+      ignore = ignore - 1
+    else
+      if color == "white" then
+        turtle.turnRight()
+      elseif color == "black" then
+        turtle.turnLeft()
+      elseif color == "yellow" then
+        ignore = ignore + 1
+      elseif color == "purple" then
+        break
+      elseif color == "blue" then
+        break
+      elseif color == "red" then
+        break
+      end
     end
   end
 end
