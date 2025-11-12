00f92f9317ad47fec3b8022053c6197573fe167f
Shane <vandesm14@gmail.com>
2025-11-12 01:58:40 -0500

    [utils]: add receipt generator

---
+++ pkgs/utils/receipt.bin.lua
@@ -0,0 +1,40 @@
+--- @type [string, string][]
+local members = {}
+
+while true do
+  print("Member:")
+  local member = read()
+  if member == "" then
+    break
+  end
+
+  print("Provides:")
+  local provides = read()
+  if provides == "" then
+    provides = "XXXXX"
+  end
+
+  table.insert(members, { member, provides })
+end
+
+local fileTimestamp = os.date("%Y%m%d_%H%M", os.epoch("utc") / 1000)
+local fileName = fileTimestamp .. ".txt"
+
+local receiptTimestamp = os.date("%Y/%m/%d at %H:%M", os.epoch("utc") / 1000)
+
+local f = fs.open(fileName, "w")
+if f ~= nil then
+  f.writeLine("Receipt of Trade")
+  f.writeLine("----------------")
+  for _, member in pairs(members) do
+    f.writeLine("")
+    f.writeLine("Member: " .. member[1])
+    f.writeLine("Provides: " .. member[2])
+  end
+  f.writeLine("")
+  f.writeLine("Date (utc): " .. receiptTimestamp)
+
+  f.close()
+end
+
+print("Saved to " .. fileName)
