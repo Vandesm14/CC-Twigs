c8d1dbc528341b9b0a83ebc747fdc6042e46d73c
Shane <vandesm14@gmail.com>
2025-11-16 17:31:22 -0500

    [turt]: use new shape lib

--- pkgs/turt/shape.lua
+++ pkgs/turt/shape.lua
@@ -1,3 +1,5 @@
+-- See the footer for licence information.
+
 --- Contains functions for moving turtle and acting upon each block in a shape.
 local shape = {}
 
@@ -55,3 +57,17 @@ function shape.area(x, z, action)
 end
 
 return shape
+
+-- Zero-Clause BSD License
+--
+-- Copyright (C) 2025 Leon Davis <leonskij@proton.me>
+--
+-- Permission to use, copy, modify, and/or distribute this software for any purpose with or without
+-- fee is hereby granted.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+-- SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+-- AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+-- NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+-- OF THIS SOFTWARE.
