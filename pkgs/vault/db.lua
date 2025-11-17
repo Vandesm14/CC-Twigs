dbd6aebed9824904caa861150f2100c805da8629
Shane <vandesm14@gmail.com>
2025-11-16 20:16:31 -0500

    [vault]: change data structure for slots

--- pkgs/vault/db.lua
+++ pkgs/vault/db.lua
@@ -2,9 +2,9 @@ local tbl = require "/pkgs.lib.table"
 
 local db = {}
 
---- @alias Slot { name: string, nbt: string, count: number, chest_id: string, slot_id: number }
---- @alias EmptySlot { chest_id: string, slot_id: number }
---- @alias Database { slots: Slot[], empty: EmptySlot[], maxCounts: table<string, number>, counts: table<string, number> }
+--- @alias Slot { name: string, nbt: string, count: number }
+--- @alias Slots table<string, Slot[]>
+--- @alias Database { slots: Slots, maxCounts: table<string, number>, counts: table<string, number> }
 
 --- Create a new Database instance.
 --- @return Database
@@ -21,49 +21,87 @@ end
 --- @param database Database
 --- @param filter number[]|nil Filter chest IDs.
 function db.scanSlots(database, filter)
-  --- @type Slot[]
+  --- @type Slots
   local slots = {}
-  --- @type EmptySlot[]
-  local empty_slots = {}
 
   local names = peripheral.getNames()
   for _, name in pairs(names) do
     local chest = peripheral.wrap(name)
+    -- If not nil and is an inventory (has `.list`).
     if chest ~= nil and chest.list ~= nil then
-      if filter == nil or (filter ~= nil and tbl.contains(filter, name)) then
-        if name ~= nil then
-          local list = chest.list()
-          for slot_id = 1, chest.size(), 1 do
-            local item = list[slot_id]
-            if item ~= nil then
-              table.insert(slots, {
-                name = item.name,
-                nbt = item.nbt,
-                count = item.count,
-                slot_id = slot_id,
-                chest_id = name,
-              })
+      local coll = {}
 
-              if database.maxCounts[item.name] == nil then
-                local detail = chest.getItemDetail(slot_id)
-                if detail ~= nil then
-                  database.maxCounts[item.name] = detail.maxCount
-                end
+      -- If no filter or is within our filter.
+      if filter == nil or (filter ~= nil and tbl.contains(filter, name)) then
+        local list = chest.list()
+        for slot_id = 1, chest.size(), 1 do
+          local item = list[slot_id]
+          if item ~= nil then
+            table.insert(coll, {
+              name = item.name,
+              nbt = item.nbt,
+              count = item.count,
+            })
+
+            if database.maxCounts[item.name] == nil then
+              local detail = chest.getItemDetail(slot_id)
+              if detail ~= nil then
+                database.maxCounts[item.name] = detail.maxCount
               end
-            else
-              table.insert(empty_slots, {
-                slot_id = slot_id,
-                chest_id = name,
-              })
             end
+          else
+            table.insert(coll, {
+              name = "",
+              nbt = "",
+              count = 0,
+            })
           end
         end
       end
+
+      slots[name] = coll
     end
   end
 
   tbl.merge(database.slots, slots)
-  tbl.merge(database.empty, empty_slots)
+end
+
+---comment
+---@param database Database
+---@param chest_id string
+---@param slot_id number
+---@return Slot|nil
+function db.querySlot(database, chest_id, slot_id)
+  local slot = database.slots[chest_id][slot_id]
+  if slot and slot.count == 0 then
+    return nil
+  end
+
+  return slot
+end
+
+--- Transfer count from an inventory into another inventory.
+--- @param count number count of items to transfer
+--- @param from_chest string the name of the inventory to transfer from
+--- @param from_slot number the slot of the inventory to transfer from
+--- @param to_chest string the name of the inventory to transfer to
+--- @param to_slot number the slot of the inventory to transfer to
+--- @return boolean success
+function db.transfer(count, from_chest, from_slot, to_chest, to_slot)
+  local destination = peripheral.wrap(to_chest)
+  if destination ~= nil then
+    local success, _ = pcall(
+      destination.pullItems,
+      from_chest,
+      from_slot,
+      count,
+      to_slot
+    )
+
+    return success
+  end
+
+  return false
 end
 
 return db
--- pkgs/vault/db.test.lua
+++ pkgs/vault/db.test.lua
@@ -1,5 +1,7 @@
 local test = require "/pkgs.lib.test"
 local db = require "/pkgs.vault.db"
+local file = require "/pkgs.lib.file"
+local tbl = require "/pkgs.lib.table"
 
 periphemu.create("back", "modem", 0)
 periphemu.create("minecraft:chest_0", "chest")
@@ -7,12 +9,29 @@ periphemu.create("minecraft:chest_1", "chest")
 periphemu.create("minecraft:chest_2", "chest")
 
 test.describe("scan tests", function()
-  test.it("hey", function()
+  test.it("new db", function()
     local database = db.new()
 
     db.scanSlots(database)
 
-    assert(#database.slots == 0)
-    assert(#database.empty == 9 * 3 * 3)
+    assert(tbl.len(database.slots))
+  end)
+end)
+
+test.describe("transfer tests", function()
+  test.it("transfer stack", function()
+    local database = db.new()
+    local cobblestone = { name = "minecraft:cobblestone", count = 64 }
+    peripheral.wrap("minecraft:chest_0").setItem(1, cobblestone)
+
+    db.scanSlots(database)
+    assert(tbl.deepEqual(db.querySlot(database, "minecraft:chest_0", 1), cobblestone))
+    assert(db.querySlot(database, "minecraft:chest_1", 1) == nil)
+
+    assert(db.transfer(64, "minecraft:chest_0", 1, "minecraft:chest_1", 1))
+    db.scanSlots(database)
+
+    assert(tbl.deepEqual(db.querySlot(database, "minecraft:chest_0", 110), nil))
+    assert(tbl.deepEqual(db.querySlot(database, "minecraft:chest_1", 1), cobblestone))
   end)
 end)
